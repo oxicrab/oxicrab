5bf5a01751700ebdb5c09244dc5cea306918bed1
James Turnbull <james@ltl.so>
2026-02-08 11:08:13 -0500

    WA spam more

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,9 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .init();
+    let filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| "info,whatsapp_rust=warn".parse().unwrap());
+    tracing_subscriber::fmt().with_env_filter(filter).init();
 
     cli::run().await
 }
